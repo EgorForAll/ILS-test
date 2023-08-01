ee44bc4891297c7a8debad5f4d7edc71b4b20f5a
EgorForAll <eia.web.ss@gmail.com>
2023-08-01 20:08:46 +0300

    osr works

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useRef } from "react";
-import leaflet from "leaflet";
-import polyline from "@mapbox/polyline";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import "leaflet-routing-machine";
+import polyline from "@mapbox/polyline";
 import marker from "../../assets/marker.svg";
 
 export const LeafletParameters = {
@@ -13,12 +14,13 @@ const Map = ({ route }) => {
   const mapRef = useRef();
 
   useEffect(() => {
-    mapRef.current = leaflet.map(`map`, {
+    const middlePoint = route.way[Math.floor(route.way.length / 2)];
+    mapRef.current = L.map(`map`, {
       center: {
-        lat: route.way[1].lat,
-        lng: route.way[1].lng,
+        lat: middlePoint.lat,
+        lng: middlePoint.lng,
       },
-      zoom: 10,
+      zoom: 12,
     });
     return () => {
       mapRef.current.remove();
@@ -26,16 +28,14 @@ const Map = ({ route }) => {
   }, [route]);
 
   useEffect(() => {
-    leaflet
-      .tileLayer(LeafletParameters.TILE_LAYER, {
-        attribution: LeafletParameters.ATTRIBUTION,
-      })
-      .addTo(mapRef.current);
+    L.tileLayer(LeafletParameters.TILE_LAYER, {
+      attribution: LeafletParameters.ATTRIBUTION,
+    }).addTo(mapRef.current);
 
-    const markerGroup = leaflet.layerGroup().addTo(mapRef.current);
+    const markerGroup = L.layerGroup().addTo(mapRef.current);
 
     route.way.forEach((point) => {
-      const customIcon = leaflet.icon({
+      const customIcon = L.icon({
         iconUrl: marker,
         iconRetinaUrl: marker,
         iconAnchor: [30, 63],
@@ -43,22 +43,21 @@ const Map = ({ route }) => {
         shadowUrl: null,
         shadowSize: null,
         shadowAnchor: null,
-        iconSize: new leaflet.Point(60, 75),
+        iconSize: new L.Point(60, 75),
         className: "map-icon",
       });
 
-      leaflet
-        .marker(
-          {
-            lat: point.lat,
-            lng: point.lng,
-          },
-          {
-            icon: customIcon,
-          }
-        )
+      L.marker(
+        {
+          lat: point.lat,
+          lng: point.lng,
+        },
+        {
+          icon: customIcon,
+        }
+      )
         .addTo(markerGroup)
-        .bindPopup(route.name);
+        .bindPopup(point.lat);
     });
 
     return () => {
@@ -67,13 +66,26 @@ const Map = ({ route }) => {
   }, [route]);
 
   useEffect(() => {
-    leaflet.Routing.control({
-      waypoints: [
-        leaflet.latLng(59.84660399, 30.29496392),
-        leaflet.latLng(59.82934196, 30.42423701),
-      ],
-    }).addTo(mapRef.current);
-  });
+    const createUrl = () => {
+      let points = [];
+      for (let point of route.way) {
+        points.push(`${point.lng},${point.lat}`);
+      }
+      let coords = points.join(";");
+
+      return `http://router.project-osrm.org/route/v1/driving/${coords}?overview=full`;
+    };
+    const url = createUrl();
+    fetch(url, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        L.polyline(polyline.decode(res.routes[0].geometry)).addTo(
+          mapRef.current
+        );
+      });
+  }, [route]);
 
   return (
     <div id="map" ref={mapRef} style={{ width: "100%", height: "700px" }}></div>
